05b0cdda9e3658929a69c40536141aff0c4d3767
Pramish Maharjan <pramish143mhzn@gmail.com>
2025-10-26 21:23:48 +0545

    default page redirect to clients page.

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClientController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/clients');
 });
 
 // List of all clients
